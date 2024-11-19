6a1ebd7e8654677e76c4e5132d8a09864887a862
trinityvthompson <trinityvthompson@utexas.edu>
2024-11-19 11:19:55 -0600

    Update maximum_profit.py

--- maximum_profit.py
+++ maximum_profit.py
@@ -1,8 +1,28 @@
+"""
+Dynamic Programming Implementation
+
+  File: maximum_profit.py
+  Description: This program finds the maximum possible profit of purchasing a subset of the
+  listed houses inputted via houses.txt file using dynamic programming.
+
+  Student Name: Marissa Shuchart
+  Student UT EID: ms87339
+
+  Partner Name: Trinity Thompson
+  Partner UT EID: tyt242
+
+  Course Name: CS 313E
+  Unique Number: 50165
+  Date Created: 11/21/2024
+  Date Last Modified: 11/21/2024
+
+"""
+
 import sys
 
 """
-This function finds the maximum possible profit of purchasing a subset of the 
-listed houses inputted via houses.txt file using dynamic programming.
+This function finds the maximum possible profit of purchasing a subset of the listed houses
+using dynamic programming (based off of the knapsack example in class).
 """
 def max_profit(money, num_houses, prices, increase):
     # create a dynamic programming table where dp_table[i][w] stores the max profit
