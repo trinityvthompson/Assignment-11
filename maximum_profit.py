b626e6efcdf1c06ed36ab21d9f03da2ebeb6ef49
trinityvthompson <trinityvthompson@utexas.edu>
2024-11-19 11:09:47 -0600

    Update maximum_profit.py

--- maximum_profit.py
+++ maximum_profit.py
@@ -11,33 +11,28 @@ def max_profit(money, num_houses, prices, increase):
             if prices[i - 1] <= w:
                 # calculate the profit from selecting the house
                 profit = prices[i - 1] * (increase[i - 1] / 100)
-                # either don't pick the house or pick it and add its profit 
+                # either don't pick the house or pick it and add its profit
                 dp_table[i][w] = max(dp_table[i - 1][w], profit + dp_table[i - 1][w - prices[i - 1]])
             else:
                 # if the house price exceeds the current budget, drop it
                 dp_table[i][w] = dp_table[i - 1][w]
-    
+
     # the maximum profit will be in the dp_table[num_houses][money]
     return dp_table[num_houses][money]
 
-# You are allowed to change the main function. 
-
-# Do not change the template file name. 
-
 def main():
-
     # The first line is the amount of investment in million USD which is an integer number.
     line = sys.stdin.readline()
     line = line.strip()
     money = int(line)
 
-
-# The second line includes an integer number which is the number of houses listed for sale.
+    # The second line includes an integer number which is the number of houses listed for sale.
     line = sys.stdin.readline()
     line = line.strip()
     num_houses = int(line)
-    
-    # The third line is a list of house prices in million dollar which is a list of \textit{integer numbers} (Consider that house prices can be an integer number in million dollar only).
+
+    # The third line is a list of house prices in million dollar which is a list of 
+    # \textit{integer numbers} (Consider that house prices can be an integer number in million dollar only).
     line = sys.stdin.readline()
     line = line.strip()
     prices = line.split(",")
@@ -51,10 +46,8 @@ def main():
     for i in range(0, len(increase)):
         increase[i] = float(increase[i])
 
-# Add your implementation here .... 
-
-# Add your functions and call them to generate the result. 
+# Add your functions and call them to generate the result.
     result = max_profit(money, num_houses, prices, increase)
     print(f"{result:.2f}")
-    
+
 main()
